76b4a0673c735f50214b54b752e83e8fc711a88b
md-mannan <mdmannan580@gmail.com>
2024-08-17 10:39:05 +0300

    printing on php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-echo '<h2>Hello World</h2>';
-printf('hello world');
+echo '<h2>Hello World</h2>'; //not support multiple value
+// print('hello world', 'I am Mannan'); //not support multiple value
+print('hello world</br>'); //not support multiple value
+printf('hello world', 'I am Mannan'); // support multiple value
 print_r("<h2>
 helo
 World my Name 
