3b895a2e12d3afb91d64dca0d8efd56af5ff02bc
md-mannan <mdmannan580@gmail.com>
2024-08-17 10:50:14 +0300

    Declaring Variables on php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-echo '<h2>Hello World</h2>'; //not support multiple value
-// print('hello world', 'I am Mannan'); //not support multiple value
-print('hello world</br>'); //not support multiple value
-printf('hello world', 'I am Mannan'); // support multiple value
-print_r("<h2>
-helo
-World my Name 
-    is Mannan
-</h2>");
+// Write Way
+$firstname = '';
+$_firstname = '';
+$first_name = '';
+$firstName = '';
+$firstname99 = '';
+// Wrong way
+// $99firstname = '';
+// $&firstname = '';
+// $first%name = '';
