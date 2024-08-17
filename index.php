86c4345a05e4eecd114748891477ec7171895482
md-mannan <mdmannan580@gmail.com>
2024-08-17 11:47:17 +0300

    PHP constant Declear

--- index.php
+++ index.php
@@ -1,37 +1,6 @@
 <?php
-// php datatypes
-$dataone = "Text";
-var_dump($dataone);
-echo '</br>';
+// php Constant
 
-$datatwo = 2;
-var_dump($datatwo);
+define('test', 50); //constant value never changeable
 echo '</br>';
-
-$datathree = 2.200;
-var_dump($datathree);
-echo '</br>';
-
-$datatfour = true;
-var_dump($datatfour);
-echo '</br>';
-
-$datatfive = array('apple', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange');
-var_dump($datatfive);
-echo '</br>';
-
-class MyClass
-{
-    public function my()
-    {
-        return "Hello World";
-    }
-}
-$datatsix = new MyClass();
-var_dump($datatsix);
-echo '</br>';
-var_dump($datatsix->my());
-echo '</br>';
-$datatseven = null;
-var_dump($datatseven);
-echo '</br>';
+echo test;
