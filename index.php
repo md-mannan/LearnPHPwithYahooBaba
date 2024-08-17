4c10c4c2008a24a3989c5007874e0e20ee25bf89
md-mannan <mdmannan580@gmail.com>
2024-08-17 12:24:04 +0300

    php Arauthmetics Operation

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
-// php Constant
+// php Arauthmetics Operation
 
-define('test', 50); //constant value never changeable
-echo '</br>';
-echo test;
+$a = 5;
+$b = 3;
+$a++;
+
+echo $a;
+echo "</br>";
+$c = $a ** $b;
+echo $c;
+echo "</br>";
+$x = 50;
+$y = 2;
+
+$z = ($x ** $y) ** $x;
+echo "</br>";
+echo ($x + $y) * $y;
+echo "</br>";
+echo $z;
+echo "</br>";
+echo ($x % $y) === 0 ?  "Even" : "odd";
+echo "</br>";
