a9222035da28d4114ead503387557b5bd600e479
md-mannan <mdmannan580@gmail.com>
2024-08-17 23:19:24 +0300

    array_search method to search item it returns position of item

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 $fruits = ['Apple', 'Mango', 'Banana', 'Lime', 'Orange', 'Abocado', 'Grape', 'Lichi', 'Pinaple', 55];
-echo in_array("55", $fruits, true) ?  "Found Succesfully" : "Cannot found";
+
+echo array_search('Lime', $fruits);
